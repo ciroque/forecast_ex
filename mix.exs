0edb8c03f8146ed8d9ff3244cf0985700b92170b
steve wagner <ciroque@live.com>
2023-12-21 14:35:20 -0800

    Fixes version and typo

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ForecastEx.MixProject do
 
   def project do
     [
-      app: :forecase_ex,
+      app: :forecast_ex,
       version: "0.1.0",
       elixir: "~> 1.14.5",
       start_permanent: Mix.env() == :prod,
